eadc00d2fe3ef14107007e08747f1d1ffdedd947
Aditya Pandey <adityapandey.inbox@gmail.com>
2025-08-04 21:12:36 +0530

    ghostty floating script made to work

--- Scripts/ghostty-launch.sh
+++ Scripts/ghostty-launch.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-WINDOWS=$(hyprctl activeworkspace -j | jq '.windows')
+WINDOWS=$(hyprctl activeworkspace -j | jq '.windows' 2>/dev/null)
 
-if [[ "$WINDOWS" -eq 0 ]]; then
-    exec ghostty
+if [[ -n "$WINDOWS" && "$WINDOWS" -gt 0 ]]; then
+    exec ghostty --class=com.mitchellh.ghostty.float
 else
-    exec ghostty --class ghostty-float
+    exec ghostty
 fi
