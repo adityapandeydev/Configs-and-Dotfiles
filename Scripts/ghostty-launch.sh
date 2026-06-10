203808a415db9372615495504f368bd5a89a1bd3
Aditya Pandey <adityapandey.inbox@gmail.com>
2026-06-10 09:13:31 +0530

    nix branch created

--- Scripts/ghostty-launch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-WINDOWS=$(hyprctl activeworkspace -j | jq '.windows' 2>/dev/null)
-
-if [[ -n "$WINDOWS" && "$WINDOWS" -gt 0 ]]; then
-    exec ghostty --class=com.mitchellh.ghostty.float
-else
-    exec ghostty
-fi
