42018bffd4dcf4d0daccd04c15252593eba20e0b
Aditya Pandey <adityapandey.inbox@gmail.com>
2026-06-13 15:53:08 +0530

    Implement packages.nix for user applications

---
+++ home/packages.nix
@@ -0,0 +1,55 @@
+{ pkgs, inputs, ... }:
+
+{
+  home.packages = with pkgs; [
+    # Development
+    jdk
+    go
+    python312
+    rustup
+    rustc
+    gcc
+    nodejs
+    gleam
+    zig_0_12
+
+    # Databases
+    postgresql
+    mysql80
+
+    # Applications
+    discord
+    vscode-fhs
+    zed-editor
+    inputs.zen-browser.packages."${pkgs.system}".default
+    protonvpn-gui
+    signal-desktop
+    vlc
+    gimp
+    floorp
+
+    # Utilities
+    btop
+    fastfetch
+    lazygit
+    neovim
+    rofi-wayland
+    lsd
+    bat
+    fzf
+    yazi
+    unzip
+    overskride
+    nixd
+    zinit
+    pywal
+
+    # Wayland tools
+    wl-clipboard
+    cliphist
+    networkmanagerapplet
+    blueman
+    brightnessctl
+    wireplumber
+  ];
+}
