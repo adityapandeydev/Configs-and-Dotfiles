3e97d1d1ae4c4433312e8e816814c8ccebc9364b
Aditya Pandey <adityapandey.inbox@gmail.com>
2026-06-19 12:00:00 +0530

    Enable mini.animate in Neovim for cursor animations

--- nvim/init.lua
+++ nvim/init.lua
@@ -779,6 +779,9 @@ require('lazy').setup({
       -- - sr)'  - [S]urround [R]eplace [)] [']
       require('mini.surround').setup()
 
+      -- Animate cursor and scrolling
+      require('mini.animate').setup()
+
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
       --  and try some other statusline plugin
